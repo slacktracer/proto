19b65083e0958783bbff3d3e8702f17d5e4b375d
slacktracer <slacktracer@gmail.com>
2020-03-25 21:02:59 -0300

    Testing CI.

---
+++ src/home/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <div>
+      <p>
+        {{ greeting }} ({{ val }}) World! {{ version }}::{{ envvar }}::{{
+          stuff("", 10)
+        }}
+      </p>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { version } from "../../package.json";
+import x from "../test";
+import HelloWorld from "./components/hello-world.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  data(): Record<string, any> {
+    return {
+      envvar: process.env.DATA,
+      greeting: "Hello",
+      version
+    };
+  },
+  computed: {
+    val: (): number => x()
+  },
+  methods: {
+    stuff: function stuff(): string {
+      return "test";
+    }
+  }
+};
+</script>
---
+++ src/test.ts
@@ -0,0 +1,9 @@
+export default (): number => 42;
+
+export const y = (): string => "";
+
+export const z = (): void => {
+  let i = 0;
+  i++;
+  i;
+};
